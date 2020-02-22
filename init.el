63bfc2d2277c9b1a5f0957c1dc306b7e43f2ccc4
LaiHan <lai.han@wootion.com>
2020-02-22 14:18:20 +0800

    change .emacs into profile.el, fix issue of init.el

--- init.el
+++ init.el
@@ -1,15 +1,3 @@
 (if (string-equal system-type "windows-nt")
-    (load "d:/edit/emacs/.emacs.d/mylisp/.emacs")
-    (load "~/.emacs.d/mylisp/.emacs"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (dash smartparens expand-region company avy))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+    (load "d:/edit/emacs/.emacs.d/mylisp/profile.el")
+    (load "~/.emacs.d/mylisp/profile.el"))
